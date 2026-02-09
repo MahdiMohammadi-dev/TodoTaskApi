c47f0d842589b05cc2119f9f405d1d0d4c6bb8a0
Mahdi_Mohammadi <jnsm89@yahoo.com>
2026-02-10 01:24:49 +0330

    add validation for title

--- Todo.Domain/DTO/CreateTaskDto.cs
+++ Todo.Domain/DTO/CreateTaskDto.cs
@@ -1,6 +1,9 @@
-﻿namespace Todo.Domain.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Domain.DTO;
 
 public class CreateTaskDto
 {
+    [Required(ErrorMessage = "عنوان نباید خالی باشد")]
     public string Title { get; set; }
 }
--- Todo.Domain/DTO/UpdateTaskDto.cs
+++ Todo.Domain/DTO/UpdateTaskDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Todo.Domain.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.Domain.DTO;
 
 public class UpdateTaskDto
 {
+    [Required(ErrorMessage = "عنوان نباید خالی باشد")]
     public string Title { get; set; }
     public bool IsCompleted { get; set; }
 }
